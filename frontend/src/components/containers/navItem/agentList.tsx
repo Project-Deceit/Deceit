90986f8321d7e18c343e05f078bcd234f1a14f24
YepW <jeffier2015@gmail.com>
2025-02-11 02:13:27 +0800

    import navi sdk

--- frontend/src/components/containers/navItem/agentList.tsx
+++ frontend/src/components/containers/navItem/agentList.tsx
@@ -9,21 +9,24 @@ import {
   useAccounts,
 } from "@mysten/dapp-kit";
 import { Transaction } from "@mysten/sui/transactions";
-import { Pool, PoolConfig } from "navi-sdk/dist/types";
+import { Pool, PoolConfig, CoinInfo } from "navi-sdk/dist/types";
 import { pool, NAVX } from "navi-sdk/dist/address";
-import { depositCoin } from "navi-sdk/dist/libs/PTB";
+import { depositCoin, returnMergedCoins } from "navi-sdk/dist/libs/PTB";
 import { AppContext } from "@/context/AppContext";
 import { getNavxBalance } from "@/apis/apis";
 import { CustomDialog } from "@/components/ui/custom-dialog";
 import router from "next/router";
-
 interface AgentListProps {
   agentList: any[];
   onCreateNew: () => void;
   onEdit: (agent: any) => void;
   refreshList: () => Promise<void>;
 }
-
+interface TokenObject {
+  objectId: string;
+  type: string;
+  balance: string;
+}
 const AgentList = ({
   agentList,
   onCreateNew,
@@ -87,7 +90,6 @@ const AgentList = ({
   const handlePageChange = (page: number) => {
     setCurrentPage(page);
   };
-
   const handlePageSizeChange = (size: number) => {
     setPageSize(size);
     setCurrentPage(1);
@@ -106,7 +108,6 @@ const AgentList = ({
           body: JSON.stringify({ agentId }),
         }
       );
-
       const result = await response.json();
       if (result.data.success) {
         toast.success("Match game successfully! Waiting for other players...");
@@ -119,7 +120,6 @@ const AgentList = ({
       setStartingAgents((prev) => ({ ...prev, [agentId]: false }));
     }
   };
-
   const handleStartMatch = async (agentId: string) => {
     setStartingAgents((prev) => ({ ...prev, [agentId]: true }));
     // navi deposit dialog
@@ -150,12 +150,22 @@ const AgentList = ({
       throw new Error("Invalid pool configuration");
     }
     const tx = new Transaction();
-    const depositNavxNum = depositNavx * 1e9;
-    const [navxCoin] = tx.splitCoins(tx.gas, [depositNavxNum]);
-    console.log("navxCoin", navxCoin, depositNavxNum);
+    const depositNavxNum = Number(depositNavx * 1e9);
+
+    // merge all navx coins
+    let to_pay_navx = await getAllNAVX();
+    if (to_pay_navx.length >= 2) {
+      let baseObj = to_pay_navx[0].objectId;
+      let all_list = to_pay_navx.slice(1).map((coin: any) => coin.objectId);
+      tx.mergeCoins(baseObj, all_list);
+    }
+    let mergedCoinObject = tx.object(to_pay_navx[0].objectId);
+    // split navx coin
+    const [navxCoin] = tx.splitCoins(mergedCoinObject, [depositNavxNum]);
     if (!navxCoin) throw new Error("Failed to split NAVX coins");
     // deposit navx to pool
     await depositCoin(tx, navxPool, navxCoin, depositNavxNum);
+
     tx.setSender(account?.address);
     // sign and execute transaction
     signAndExecuteTransaction(
@@ -195,6 +205,52 @@ const AgentList = ({
         },
       }
     );
+  };
+
+  const getAllNAVX = async (
+    cursor?: string,
+    accumulated: TokenObject[] = []
+  ): Promise<TokenObject[]> => {
+    try {
+      const response = await client.getOwnedObjects({
+        owner: walletAddress as string,
+        filter: {
+          MatchAll: [
+            {
+              StructType: "0x2::coin::Coin",
+            },
+          ],
+        },
+        options: {
+          showType: true,
+          showContent: true,
+        },
+        cursor: cursor,
+      });
+
+      const currentTokens = response.data
+        .filter((obj: any) => obj.data?.content?.type?.includes("coin::Coin"))
+        .map((obj: any) => ({
+          objectId: obj.data?.objectId,
+          type: obj.data?.content?.type,
+          balance: obj.data?.content?.fields?.balance,
+        }));
+
+      const navxTokens = currentTokens.filter((token: TokenObject) =>
+        token.type.includes(NAVX.address)
+      );
+
+      const updatedAccumulated = [...accumulated, ...navxTokens];
+
+      if (response.hasNextPage && response.nextCursor) {
+        return getAllNAVX(response.nextCursor, updatedAccumulated);
+      }
+
+      return updatedAccumulated;
+    } catch (error) {
+      console.error("Failed to fetch NAVX tokens:", error);
+      throw error;
+    }
   };
 
   const renderDialogContent = () => {
